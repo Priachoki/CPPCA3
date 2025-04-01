0a5f9fba8a9efe2153dcd0122a3351bc5aa34f1a
Priyanka <priyankakwambokaachoki@gmail.com>
2025-04-01 01:06:27 +0100

    Added the constructor in the Crawler file

--- Crawler.h
+++ Crawler.h
@@ -2,15 +2,38 @@
 // Created by MyPC on 31/03/2025.
 //
 
+#include <list>
+#include "Direction.h"
+#include <iostream>
 #ifndef CRAWLER_H
 #define CRAWLER_H
 
+using namespace std;
 
-
-class Crawler {
-
+struct Position {
+    int x;
+    int y;
 };
 
 
+class Crawler {
+public:
+
+    Crawler(int id, Position pos, Direction dir, int size);
+
+
+    int id;
+    Position position;
+    Direction direction;
+    int size;
+    bool alive;
+    std::list<Position> path;
+
+    void move();
+    bool isWayBlocked() const;
+private:
+
+    Direction getRandomDirection() const;
+};
 
 #endif //CRAWLER_H
